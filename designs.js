2e1e419bd83ab31af6f7473b89a3ee79810c414e
Osemudiamen Ehiabhi <osaseharvee@gmail.com>
2018-07-01 20:41:20 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,25 +1,25 @@
 // Select color input
-var colorInput = $("#colorPicker") ;
+const colorInput = $("#colorPicker") ;
 
 // Select size input
-var gridHeight = $("#inputHeight");
-var gridWidth = $("#inputWeight");
+const gridHeight = $("#inputHeight");
+const gridWidth = $("#inputWeight");
 
 // When size is submitted by the user, call makeGrid()
 $("#sizePicker").submit(function makeGrid(grid) {
     $('table tr').remove();
-    for (var h = 1 ; h <= gridHeight.val() ; h++){
-       var row = $("table").append("<tr></tr>");
-        for (var w = 1 ; w <= gridWidth.val() ; w++){
-           var column =  $('tr:last').append("<td></td>");
+    for (let h = 1 ; h <= gridHeight.val() ; h++){
+      let row = $("table").append("<tr></tr>");
+        for (let w = 1 ; w <= gridWidth.val() ; w++){
+           let column =  $('tr:last').append("<td></td>");
             $('td').attr("class",'pixels');
-            console.log("we have " + gridHeight.val() + "row(s) and " + gridWidth.val() +"column(s)");
+            console.log("We have " + gridHeight.val() + "row(s) and " + gridWidth.val() +"column(s)");
         } 
     }  
     grid.preventDefault();
     
     $(".pixels").click(function (event){
-        var paintCells = $('#colorPicker').val();
+        let paintCells = $('#colorPicker').val();
         $(event.target).css('background-color', paintCells);
         console.log("This cell has a color code of " + paintCells);
     });
